62c85c19e654e2b12bc913f8bf5b81a40b33363a
AltTether <200351p@ugs.kochi-tech.ac.jp>
2018-05-16 01:25:25 +0900

    wip

--- babi_mn_l3_check2.py
+++ babi_mn_l3_check2.py
@@ -4,59 +4,15 @@ import sklearn.utils
 import babi_data_util
 import tensorflow as tf
 
-
 data_path = "tasks_1-20_v1-2.tar.gz"
 
 
-def create_babi_data(path, filetype="train", num_hint=1):
-    tar = tarfile.open(path)
-    challenge = 'tasks_1-20_v1-2/en/qa1_single-supporting-fact_{}.txt'
-    return babi_data_util.get_stories(tar.extractfile(challenge.format(filetype)), num_hint)
-
-def create_input_data_(inputs, ids, max_sentence_length, max_query_length):
-    max_story_length = babi_data_util.get_max_story_length_(inputs)
-    ids_inputs = convert_ids_(inputs, ids, max_story_length, max_sentence_length, max_query_length)
-
-    x = []
-    q = []
-    t = []
-    for ids_input in ids_inputs:
-        x +=[ids_input[0]]
-        q += [ids_input[1]]
-        t += ids_input[2]
-    return x, q, t
-
-def convert_ids_(inputs, ids, max_story_length, max_sentence_length, max_query_length):
-    output = []
-    for x, q, t in inputs:
-        x_ids_list = []
-        for sentence in x:
-            sentence_ids = []
-            for word in sentence:
-                sentence_ids += [ids[word]]
-            if len(sentence_ids) < max_sentence_length:
-                sentence_ids += (0 for _ in range(max_sentence_length - len(sentence_ids)))
-
-            x_ids_list += [sentence_ids]
-
-        if len(x_ids_list) < max_story_length:
-            for _ in range(max_story_length - len(x_ids_list)):
-                x_ids_list += [[0 for __ in range(max_sentence_length)]]
-
-        q_ids = []
-        for word in q:
-            q_ids += [ids[word]]
-        if len(q_ids) < max_query_length:
-            q_ids += (0 for _ in range(max_query_length - len(q_ids)))
-        t_id = [ids[t[0]]]
-        output += [[x_ids_list, q_ids, t_id]]
-    return output
-
 def inference(x, q, t, d, V, n_layer, batch_size):
     A = tf.Variable(tf.concat([tf.zeros(shape=[1, d], dtype=tf.float32), tf.truncated_normal(shape=[V, d], dtype=tf.float32)], axis=0))
+    B = tf.Variable(tf.concat([tf.zeros(shape=[1, d], dtype=tf.float32), tf.truncated_normal(shape=[V, d], dtype=tf.float32)], axis=0))
     Cn = [tf.Variable(tf.concat([tf.zeros(shape=[1, d]), tf.truncated_normal(shape=[V, d])], axis=0)) for _ in range(n_layer)]
 
-    next_u = tf.reduce_sum(tf.nn.embedding_lookup(A, q), axis=1) # shape=(batch_size, d)
+    next_u = tf.reduce_sum(tf.nn.embedding_lookup(B, q), axis=1) # shape=(batch_size, d)
     m = None
     for layer in range(n_layer):
         if layer == 0:
@@ -76,6 +32,31 @@ def inference(x, q, t, d, V, n_layer, batch_size):
 
     return a
 
+def inference_(x, q, t, d, V, n_layer, batch_size):
+    A = tf.Variable(tf.concat([tf.zeros(shape=[1, d]), tf.truncated_normal(shape=[V, d])], axis=0))
+    B = tf.Variable(tf.concat([tf.zeros(shape=[1, d]), tf.truncated_normal(shape=[V, d])], axis=0))
+    Cn = [tf.Variable(tf.concat([tf.zeros(shape=[1, d]), tf.truncated_normal(shape=[V, d])], axis=0)) for _ in range(n_layer)]
+
+    next_u = tf.reshape(tf.reduce_sum(tf.nn.embedding_lookup(B, q), axis=1), shape=[-1, d, 1])  # shape = [bs, d, 1]
+
+    for layer in range(n_layer):
+        if layer == 0:
+            A_ = A
+        else:
+            A_ = Cn[layer-1]
+        C = Cn[layer]
+
+        m = tf.transpose(tf.reduce_sum(tf.nn.embedding_lookup(A_, x), axis=2), perm=[0,2,1])    # shape = [bs, d, storu_l]
+        p = tf.nn.softmax(tf.matmul(tf.transpose(next_u, perm=[0, 2, 1]), m))                   # shape = [bs, 1, story_l]
+        c = tf.transpose(tf.reduce_sum(tf.nn.embedding_lookup(C, x), axis=2), perm=[0,2,1])     # shape = [bs, d, story_l]
+        o = tf.matmul(p, tf.transpose(c, perm=[0, 2, 1]))                                       # shape = [bs, 1, d]
+        next_u = tf.add(next_u, tf.transpose(o, perm=[0, 2, 1]))                                 # shape = [bs, d, 1]
+
+    W = tf.Variable(tf.concat([tf.zeros(shape=[d, 1]), tf.truncated_normal(shape=[d, V])], axis=1))
+    a = tf.nn.softmax(tf.matmul(tf.reshape(next_u, shape=[-1, d]), W))
+
+    return a
+
 def predict(infer_output):
     return tf.argmax(infer_output, axis=1)
 
@@ -85,6 +66,12 @@ def calc_loss(infer_output, t, V):
 def train(loss):
     optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001)
     return optimizer.minimize(loss)
+
+def train_(loss):
+    tvars = tf.trainable_variables()
+    grads, _ = tf.clip_by_global_norm(tf.gradients(loss, tvars), 40)
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001)
+    return optimizer.apply_gradients(zip(grads, tvars), global_step=tf.train.get_or_create_global_step())
 
 def calc_acc(output, t):
     prediction = tf.cast(predict(output), dtype=tf.int32)
@@ -108,8 +95,8 @@ def main():
     batch_size = 32
     n_layer = 1
 
-    train_input = create_babi_data(data_path)
-    test_input = create_babi_data(data_path, filetype="test", num_hint=1)
+    train_input = babi_data_util.create_babi_data(data_path)
+    test_input = babi_data_util.create_babi_data(data_path, filetype="test", num_hint=1)
 
     ids, ids_ = babi_data_util.create_vocab_dict_(train_input, test_input)
     V = len(ids)    # 21?
@@ -118,15 +105,15 @@ def main():
     max_sentence_length = max(map(len, (x for x, _, _, in train_input + test_input)))
     max_query_length = max(map(len, (x for _, x, _, in train_input + test_input)))
 
-    x, q, t = create_input_data_(train_input, ids, max_sentence_length, max_query_length)
-    x_test, q_test, t_test = create_input_data_(test_input, ids, max_sentence_length, max_query_length)
+    x, q, t = babi_data_util.create_input_data_(train_input, ids, max_sentence_length, max_query_length)
+    x_test, q_test, t_test = babi_data_util.create_input_data_(test_input, ids, max_sentence_length, max_query_length)
 
     X = tf.placeholder(dtype=tf.int32, shape=[None, max_story_length, max_sentence_length])
     Q = tf.placeholder(dtype=tf.int32, shape=[None, max_query_length])
     T = tf.placeholder(dtype=tf.int32, shape=[None])
     BS = tf.placeholder(dtype=tf.int32, shape=[])
 
-    a = inference(X, Q, T, d, V, n_layer, BS)
+    a = inference_(X, Q, T, d, V, n_layer, BS)
     loss = calc_loss(a, T, V)
     train_step = train(loss)
 
@@ -135,11 +122,16 @@ def main():
 
     n_batch = len(x) // batch_size
 
-    sess = tf.Session();
-    sess.run(tf.global_variables_initializer())
+    sess = tf.Session()
+    saver = tf.train.Saver()
+    ckpt = tf.train.get_checkpoint_state('./checkpoints/')
+    if ckpt:
+        last_model = ckpt.model_checkpoint_path
+    else:
+        sess.run(tf.global_variables_initializer())
 
     for epoch in range(10000000):
-        x_train, q_train, t_train, x_valid, q_valid, t_valid = split_train_validate_(x, q, t)
+        x_train, q_train, t_train, x_valid, q_valid, t_valid = babi_data_util.split_train_validate_(x, q, t)
         n_batch = len(x_train) // batch_size
         for i in range(n_batch):
             start = batch_size * i
@@ -148,10 +140,13 @@ def main():
         if epoch % 10 == 0:
             print("train_loss: {0:.10f}".format(loss.eval(session=sess, feed_dict={X: x_train, Q: q_train, T: t_train, BS: len(x_train)})),
                   "valid_loss: {0:.3f}".format(loss.eval(session=sess, feed_dict={X: x_valid, Q: q_valid, T: t_valid, BS: len(x_valid)})),
-                  "train_acc: {}".format(accuracy.eval(session=sess, feed_dict={X: x_train, Q: q_train, T: t_train, BS: len(x_train)})),
-                  "valid_acc: {}".format(accuracy.eval(session=sess, feed_dict={X: x_valid, Q: q_valid, T: t_valid, BS: len(x_valid)})))
+                  "train_acc: {0:.4f}".format(accuracy.eval(session=sess, feed_dict={X: x_train, Q: q_train, T: t_train, BS: len(x_train)})),
+                  "valid_acc: {0:.4f}".format(accuracy.eval(session=sess, feed_dict={X: x_valid, Q: q_valid, T: t_valid, BS: len(x_valid)})))
             print(prediction.eval(session=sess, feed_dict={X: x_train[0:10], Q: q_train[0:10], T: t_train[0:10], BS: len(x_train[0:10])}))
             print(t_train[0:10])
+
+        if epoch % 100 == 0 and epoch != 0:
+            saver.save(sess, "./checkpoints/model.ckpt")
 
 
 if __name__ == "__main__":
